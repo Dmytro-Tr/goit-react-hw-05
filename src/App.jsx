449088b29bbc946d9a22f57bd048a374a9500c86
Dmtro_Tr <Di4gamegem@gmail.com>
2025-01-09 18:03:05 +0200

    css settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <>Hello world!</>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<h1>Hello</h1>}
+        />
+        <Route
+          path="/about"
+          element={<h2>About</h2>}
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import s from "./header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Header</h2>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Header;
